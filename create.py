fd15a7ecde5d18ac8f84fd86e8c0dd82e426658a
Tknight <35734301+Tk369@users.noreply.github.com>
2020-08-01 18:38:56 +0800

    Update create.py

--- create.py
+++ create.py
@@ -1,7 +1,19 @@
+f1 = open("ip.txt","r+")
+arry_  = []
+for i in f1.readlines():
+	i = i.strip("\n").split(".")
+	tempdata = i[0]+"."+i[1]+"."+i[2]+"."
+	arry_.append(tempdata)
+
+
 a = 'xxx.xxx.xxx.'
 
 f = open('domain.txt','a+')
+
 for i in range(255):
-	ip = a+str(i)
-	f.write(ip+'\n')
+	# if you want input ip address please delete # ip = a...... or input "#" to ip=j+str....
+	# ip = a+str(i)
+	for j in arry_:
+		ip = j+str(i)+"\n"
+		f.write(ip+'\n')
 f.close()
