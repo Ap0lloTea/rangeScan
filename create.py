9409963b60347092a7f9eb5ffa379b35a0a2db69
Tknight <35734301+Tk369@users.noreply.github.com>
2020-08-01 18:47:02 +0800

    Update create.py

--- create.py
+++ create.py
@@ -1,12 +1,16 @@
+
 f1 = open("ip.txt","r+")
 arry_  = []
 for i in f1.readlines():
-	i = i.strip("\n").split(".")
-	tempdata = i[0]+"."+i[1]+"."+i[2]+"."
-	arry_.append(tempdata)
+	if "." in i:
+		i = i.strip("\n").split(".")
+		tempdata = i[0]+"."+i[1]+"."+i[2]+"."
+		arry_.append(tempdata)
+	else:
+		print("Open file Error!")
 
 
-a = 'xxx.xxx.xxx.'
+#a = 'xxx.xxx.xxx.'
 
 f = open('domain.txt','a+')
 
@@ -17,3 +21,4 @@ for i in range(255):
 		ip = j+str(i)+"\n"
 		f.write(ip+'\n')
 f.close()
+
